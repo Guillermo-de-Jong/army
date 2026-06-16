86ca111489df631c1c7fe5a645a05adf68c811e0
Guillermo-de-Jong <guillermo.de.jong@hva.nl>
2026-06-16 10:52:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const buildingsData = [
   { id: "amstel-hotel",           className: "gebouw-amstel-hotel",           fileName: "amstelhotel.png",                          top: "55%",       left: "24%" },
   { id: "amstelhaven",            className: "gebouw-amstelhaven",            fileName: "Amstelhaven.png",                          top: "75%",       left: "16%" },
   { id: "bb-sleep",               className: "gebouw-bb-sleep",               fileName: "B&B Sleep in Amsterdam.png",               top: "12%",       left: "19%" },
-  { id: "bakhuys",                className: "gebouw-bakhuys",                fileName: "Bakhuys Amsterdam.png",                    top: "52%",       left: "49%" },
+  { id: "bakhuys",         airtableId: "recZDEh9xnhTbAmmD",       className: "gebouw-bakhuys",                fileName: "Bakhuys Amsterdam.png",                    top: "52%",       left: "49%" },
   { id: "bar-lempicka",           className: "gebouw-bar-lempicka",           fileName: "Bar Lempicka.png",                         top: "44.6%",     left: "32.9%" },
   { id: "bistro-baret",           className: "gebouw-bistro-baret",           fileName: "Bistro Baret.png",                         top: "34.1%",     left: "45.5%" },
   { id: "bistro-bonjour",         className: "gebouw-bistro-bonjour",         fileName: "Bistro Bonjour.png",                       top: "7.5%",      left: "21.5%" },
@@ -52,6 +52,7 @@ buildingsData.forEach(building => {
   img.src = `images/${building.fileName}`;
   img.className = building.className;
   img.setAttribute("data-id", building.id);
+  img.setAttribute("data-airtable-id", building.airtableId); // <-- VOEG DEZE LIJN TOE
   img.alt = building.id;
  
   img.style.top = building.top;
@@ -77,13 +78,13 @@ setTimeout(() => {
  
 element.parentElement.addEventListener("wheel", panzoom.zoomWithWheel);
  
-// 6. FUNCTIE: STUUR ACTIE NAAR BRAVO
-function navigateInBravo(screenName, buildingId) {
+// 6. FUNCTIE: STUUR ACTIE NAAR BRAVO (Aangepast voor Bravo query parameters)
+function navigateInBravo(screenName, airtableId) {
   const message = {
     action: "goto",
     params: {
-      href: screenName,
-      buildingId: buildingId
+      // We plakken de ID achter de screenName als een query parameter
+      href: `${screenName}?buildingId=${airtableId}` 
     }
   };
  
@@ -92,7 +93,6 @@ function navigateInBravo(screenName, buildingId) {
   }
 }
 
-
 // 7. CLICK INTERACTION
 buildingsContainer.addEventListener("click", function(event) {
   const clickedImg = event.target.closest("img");
@@ -100,12 +100,14 @@ buildingsContainer.addEventListener("click", function(event) {
   if (!clickedImg) return;
  
   const buildingId = clickedImg.getAttribute("data-id");
+  const airtableId = clickedImg.getAttribute("data-airtable-id"); // <-- Haal Airtable ID op
   const isExistingLead = existingLeadIds.includes(buildingId);
  
- 
   if (isExistingLead) {
-    navigateInBravo(detailScreen, buildingId);
+    // We sturen nu de airtableId mee naar het detailscherm
+    navigateInBravo(detailScreen, airtableId);
   } else {
-    navigateInBravo(createLeadScreen, buildingId);
+    // Voor het aanmaken kun je eventueel ook de ID meesturen als je die daar nodig hebt
+    navigateInBravo(createLeadScreen, airtableId);
   }
 });
